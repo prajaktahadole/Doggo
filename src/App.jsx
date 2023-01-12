75532e2bf60ed138c162918406bd61d118ec6d4f
prajaktahadole <prajaktahadole@gmail.com>
2023-01-12 17:46:19 +0530

    login registration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Register from './Register';
+import Login from './Login';
+
+function App() {
+ 
+  return (
+    <div className="App">
+       <Login />
+       <Register/>
+    </div>
+  )
+}
+
+export default App
