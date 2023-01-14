a69af3bd06c5c57b0492635c7e5fb6b8acf13c85
prajaktahadole <prajaktahadole@gmail.com>
2023-01-14 16:56:59 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import LandingPage from './components/pages/Landing'
+import LoginPage from './components/pages/Login'
+import RegisterPage from './components/pages/Register'
+import ForgetPasswordPage from './components/pages/ForgetPassword'
+import HomePage from './components/pages/Home'
+
 import './App.css'
-import Register from './Register';
-import Login from './Login';
 
 function App() {
- 
-  return (
+ return (
     <div className="App">
-       <Login />
-       <Register/>
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={ LandingPage } />
+                    <Route path="/login" component={ LoginPage } />
+                    <Route path="/register" component={ RegisterPage } />
+                    <Route path="/forget-password" component={ ForgetPasswordPage } />
+                    <Route path="/home" component={ HomePage } />
+                </Switch>
+            </div>
+        </Router>
     </div>
   )
 }
+
 
 export default App
